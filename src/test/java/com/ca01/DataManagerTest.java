c7e7c1b3a9dd1bf6aa7059fd8a1e8362674383e8
sara.asadi22 <sara.asadi22@gmail.com>
2022-02-25 19:32:48 +0330

    VoteComment Test added

--- src/test/java/com/ca01/DataManagerTest.java
+++ src/test/java/com/ca01/DataManagerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -245,6 +246,140 @@ class DataManagerTest {
     }
 
     @Test
+    void newVoteCommentLikeTest() throws ParseException {
+        dataManager.users.add(new User("sara@ut.ac.ir"," "," "," ","2000-01-12"));
+        dataManager.movies.add(new Movie(1,"","","","",new ArrayList<>(),new ArrayList<>(),new ArrayList<>(),0,0,0));
+
+        Comment comment = new Comment(" ",1," ",1);
+
+        dataManager.movies.get(0).comments.add(comment);
+        dataManager.comments.add(comment);
+
+        String voteString = "{\"userEmail\": \"sara@ut.ac.ir\", \"commentId\": 1, \"vote\": 1}";
+        JSONObject voteObject = (JSONObject) parser.parse(voteString);
+        dataManager.voteComment(voteObject);
+
+        List<Long> vote = new ArrayList<>();
+        vote.add(1L);
+        vote.add(1L);
+
+        assertEquals(vote, dataManager.users.get(0).ratedComments.get(0));
+        assertEquals(1, dataManager.movies.get(0).comments.get(0).likes);
+    }
+
+    @Test
+    void updateVoteCommentLikeTest() throws ParseException {
+        dataManager.users.add(new User("sara@ut.ac.ir"," "," "," ","2000-01-12"));
+        dataManager.movies.add(new Movie(1,"","","","",new ArrayList<>(),new ArrayList<>(),new ArrayList<>(),0,0,0));
+
+        Comment comment = new Comment(" ",1," ",1);
+        comment.likes = 1;
+        List<Long> vote = new ArrayList<>();
+        vote.add(1L);
+        vote.add(1L);
+        dataManager.movies.get(0).comments.add(comment);
+        dataManager.comments.add(comment);
+        dataManager.users.get(0).ratedComments.add(vote);
+
+        String voteString = "{\"userEmail\": \"sara@ut.ac.ir\", \"commentId\": 1, \"vote\": -1}";
+        JSONObject voteObject = (JSONObject) parser.parse(voteString);
+        dataManager.voteComment(voteObject);
+
+        List<Long> updateVote = new ArrayList<>();
+        updateVote.add(1L);
+        updateVote.add(-1L);
+
+        assertEquals(updateVote, dataManager.users.get(0).ratedComments.get(0));
+        assertEquals(0, dataManager.movies.get(0).comments.get(0).likes);
+        assertEquals(1, dataManager.movies.get(0).comments.get(0).disLikes);
+    }
+
+    @Test
+    void undoVoteCommentLikeTest() throws ParseException {
+        dataManager.users.add(new User("sara@ut.ac.ir"," "," "," ","2000-01-12"));
+        dataManager.movies.add(new Movie(1,"","","","",new ArrayList<>(),new ArrayList<>(),new ArrayList<>(),0,0,0));
+
+        Comment comment = new Comment(" ",1," ",1);
+        comment.likes = 1;
+        List<Long> vote = new ArrayList<>();
+        vote.add(1L);
+        vote.add(1L);
+        dataManager.movies.get(0).comments.add(comment);
+        dataManager.comments.add(comment);
+        dataManager.users.get(0).ratedComments.add(vote);
+
+        String voteString = "{\"userEmail\": \"sara@ut.ac.ir\", \"commentId\": 1, \"vote\": 0}";
+        JSONObject voteObject = (JSONObject) parser.parse(voteString);
+        dataManager.voteComment(voteObject);
+
+        List<Long> updateVote = new ArrayList<>();
+        updateVote.add(-1L);
+        updateVote.add(1L);
+
+        assertEquals(updateVote, dataManager.users.get(0).ratedComments.get(0));
+        assertEquals(0, dataManager.movies.get(0).comments.get(0).likes);
+        assertEquals(0, dataManager.movies.get(0).comments.get(0).disLikes);
+    }
+
+    @Test
+    void voteCommentUserNotFountTest() throws ParseException {
+        dataManager.movies.add(new Movie(1,"","","","",new ArrayList<>(),new ArrayList<>(),new ArrayList<>(),0,0,0));
+
+        Comment comment = new Comment(" ",1," ",1);
+
+        dataManager.movies.get(0).comments.add(comment);
+        dataManager.comments.add(comment);
+
+        String voteString = "{\"userEmail\": \"sara@ut.ac.ir\", \"commentId\": 1, \"vote\": -1}";
+        JSONObject voteObject = (JSONObject) parser.parse(voteString);
+        int error = dataManager.voteComment(voteObject);
+
+        assertEquals(-1, error);
+        assertEquals(0, dataManager.movies.get(0).comments.get(0).likes);
+        assertEquals(0, dataManager.movies.get(0).comments.get(0).disLikes);
+    }
+
+    @Test
+    void voteCommentInvalidValueTest() throws ParseException {
+        dataManager.users.add(new User("sara@ut.ac.ir"," "," "," ","2000-01-12"));
+        dataManager.movies.add(new Movie(1,"","","","",new ArrayList<>(),new ArrayList<>(),new ArrayList<>(),0,0,0));
+
+        Comment comment = new Comment(" ",1," ",1);
+
+        dataManager.movies.get(0).comments.add(comment);
+        dataManager.comments.add(comment);
+
+        String voteString = "{\"userEmail\": \"sara@ut.ac.ir\", \"commentId\": 1, \"vote\": 2}";
+        JSONObject voteObject = (JSONObject) parser.parse(voteString);
+        int error = dataManager.voteComment(voteObject);
+
+        assertEquals(-3, error);
+        assertEquals(0, dataManager.movies.get(0).comments.get(0).likes);
+        assertEquals(0, dataManager.movies.get(0).comments.get(0).disLikes);
+    }
+
+    @Test
+    void voteCommentCommentNotFoundTest() throws ParseException {
+        dataManager.users.add(new User("sara@ut.ac.ir"," "," "," ","2000-01-12"));
+        dataManager.movies.add(new Movie(1,"","","","",new ArrayList<>(),new ArrayList<>(),new ArrayList<>(),0,0,0));
+
+        Comment comment = new Comment(" ",1," ",2);
+
+        dataManager.movies.get(0).comments.add(comment);
+        dataManager.comments.add(comment);
+
+        String voteString = "{\"userEmail\": \"sara@ut.ac.ir\", \"commentId\": 1, \"vote\": 0}";
+        JSONObject voteObject = (JSONObject) parser.parse(voteString);
+        int error = dataManager.voteComment(voteObject);
+
+        assertEquals(-2, error);
+        assertEquals(0, dataManager.movies.get(0).comments.get(0).likes);
+        assertEquals(0, dataManager.movies.get(0).comments.get(0).disLikes);
+    }
+
+
+
+    @Test
     void userAddMovieToWatchListTest() throws ParseException, java.text.ParseException {
         dataManager.users.add(new User("sara@ut.ac.ir","","","","2000-01-10"));
         dataManager.movies.add(new Movie(1,"","","","",new ArrayList<>(),new ArrayList<>(),new ArrayList<>(),0,0,0));
@@ -348,7 +483,6 @@ class DataManagerTest {
         assertEquals(-1, error);
         assertTrue(dataManager.users.get(0).watchList.size() == 1);
     }
-
 
     @Test
     void GetMovieListEmpty(){
