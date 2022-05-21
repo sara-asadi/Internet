1c6592cffcc4b43501f9bcc953a9cf7c5b3ee5d0
mazdarani <80466988+mazdarani@users.noreply.github.com>
2022-05-21 22:48:34 +0430

    SignUp added

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,31 +1,58 @@
-
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import React from 'react';
 // import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 //import Movies from "./pages/movies/Movies";
 import Actor from './pages/actor/Actor';
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/actors" element={<Actor />} />
-        {/*<Route path="/movies" element={<Movies />} />
-        
-        <Route path="/signup" element={<Signup />} />
-        {/* <Route exact path="/" component={Home} /> */}
-      </Routes>
-    </Router>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      jwt: null
+    };
+  }
 
-// function Logout(){
-//   return (<div>
-//     HI
-//     </div>);
-// }
+  render() {
+    const loadingTag = () => {
+      if (this.state.loading) {
+        return (
+          <div className="loading" >
+            <div className="spinner-grow" role="status">
+            </div>
+          </div>);
+      }
+      else {
+        return null;
+      }
+    }
+
+    const setLoading = (v) => {
+      this.setState({ loading: v });
+    }
+
+    return (
+      <div className="wrapper">
+        {loadingTag()}
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/actors" element={<Actor />} />
+            {/*<Route path="/movies" element={<Movies />} />
+        
+        
+        {/* <Route exact path="/" component={Home} /> */}
+          </Routes>
+        </Router >
+        <ToastContainer />
+      </div>
+    );
+  }
+}
 
 export default App;
